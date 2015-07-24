e04f4b06862ccc9ecf05a29cf272f96862f895a4
Peter <peter@sonictrio.com>
2015-07-24 20:22:04 +0100

    App manifest has wide tile added, so you can pick the wide tile size. Two title sizes have been added to demo using the two different sizes. The background image needs to added as the first element to the binding, so I have added it to the object to be added that way. Now, I dunno if this is the best way of doing it, but it works. I have tweaked the demo square option as it was the new subgroup was forcing the tile to only show the image. Then I have also added a couple more items to the alignement and branding enums as they are options in the http://blogs.msdn.com/b/tiles_and_toasts/archive/2015/06/30/adaptive-tile-templates-schema-and-documentation.aspx and I was looking at them when trying to stop the image dominating the tile.

--- AdaptiveTileExtensionsSample/MainPage.xaml.cs
+++ AdaptiveTileExtensionsSample/MainPage.xaml.cs
@@ -22,27 +22,61 @@ namespace AdaptiveTileExtensionsSample
         private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
         {
             var tile = AdaptiveTile.CreateTile();
-            var binding = TileBinding.Create(TemplateType.TileWide);
+            
+            #region for wide tile with background 
+            
+            var wideBinding = TileBinding.Create(TemplateType.TileWide);
+            wideBinding.Branding = Branding.NameAndLogo;
+            var wideLogo = new TileImage(ImagePlacement.Background)
+            {
+                Source = "http://fc02.deviantart.net/fs71/i/2013/359/a/4/deadpool_logo_1_fill_by_mr_droy-d5q6y5u.png"
+            };
+            //background/peek images need to be at the root of the binding;
+            wideBinding.BackgroundImage = wideLogo;
+
+            var wideHeader = new Text("You have mail") {Style = TextStyle.Body};
+            var wideContent = new Text("Someone likes you!") {Style = TextStyle.Caption};
+
+            var wideSubgroup = new SubGroup();
+            wideSubgroup.AddText(wideHeader);
+            wideSubgroup.AddText(wideContent);
+            
+            wideBinding.AddSubgroup(wideSubgroup, true);
+
+            #endregion
+
+            #region square tile 
+           
+            var binding = TileBinding.Create(TemplateType.TileMedium);
             binding.Branding = Branding.None;
 
-            var header = new Text("You have mail") {Style = TextStyle.Body};
-            var content = new Text("Someone likes you!") {Style = TextStyle.Caption};
+            var header = new Text("You have mail") { Style = TextStyle.Body, WrapText = true };
+            var content = new Text("Someone likes you!") { Style = TextStyle.Caption, IsSubtleStyle = true , WrapText = true,Alignment = Alignment.Center};
             var logo = new TileImage(ImagePlacement.Inline)
             {
                 Source = "http://fc02.deviantart.net/fs71/i/2013/359/a/4/deadpool_logo_1_fill_by_mr_droy-d5q6y5u.png"
             };
 
-            var logoSubGroup = new SubGroup {Width = 40};
-            logoSubGroup.AddImage(logo);
+            var imageSubgroup = new SubGroup()
+            {
+                Width = 20
+            };
+            imageSubgroup.AddImage(logo);
+
 
             var subgroup = new SubGroup();
             subgroup.AddText(header);
             subgroup.AddText(content);
+          
+            binding.AddSubgroup(imageSubgroup);
+            binding.AddSubgroup(subgroup);
+            
+            #endregion
 
-            binding.AddSubgroup(logoSubGroup);
-            binding.AddSubgroup(subgroup, true);
+            // Add the types of tile bindings to the tile.
+            tile.Tiles.Add(wideBinding);
+            tile.Tiles.Add(binding); 
 
-            tile.Tiles.Add(binding);
             var notification = tile.GetNotification();
 
             TileUpdateManager.CreateTileUpdaterForApplication().Update(notification);
